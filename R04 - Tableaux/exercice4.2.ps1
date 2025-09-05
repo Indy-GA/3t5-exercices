24ab4d803da0372059e572f37cfbdc01bf3b672a
Indy-GA <1380029@cegepmontpetit.ca>
2025-09-05 14:28:54 -0400

    fct
    fin d'exercice 4/02

--- R04 - Tableaux/exercice4.2.ps1
+++ R04 - Tableaux/exercice4.2.ps1
@@ -3,14 +3,16 @@
 # 4.2.1. Obtenir la liste de tous les services dont le nom commence par W et afficher les 
 #        propriétés Name, Status et StartType dans un tableau.
 
-
+        Get-Service -Name w* | format-table name,status,StartType
 
 # 4.2.2. Même chose, mais montrer seulement les services en cours d'exécution.
 
-
+        Get-Service -Name w* | Where-Object -Property Status -eq "running" |format-table name,status,StartType
 
 # 4.2.3. Même chose, mais montrer seulement les services en cours d'exécution qui 
 #        s'exécutent automatiquement.
+
+        Get-Service -Name w* | Where-Object {$_.Status -eq "running" -and $_. StartType -eq "Automatic"} |format-table name,status,StartType
 
 
 
