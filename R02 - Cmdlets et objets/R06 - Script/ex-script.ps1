f94cd07ba47c2f7f500ff590e13965e7148a5450
Indy-GA <1380029@cegepmontpetit.ca>
2025-09-22 19:00:48 -0400

    progres
    ex r06 script

---
+++ R02 - Cmdlets et objets/R06 - Script/ex-script.ps1
@@ -0,0 +1,6 @@
+if (($reponse = Read-Host -Prompt "Rentrez un nombre") % 2 -eq 0) {
+        (Write-Host "Ceci est un nombre pair")
+                  }
+else {
+        (Write-Host "Ceci est un nombre impair")
+    } 
